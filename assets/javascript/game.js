bf8da79f684193fc001f54275e9893e3d7a8d866
Jonas M <jonasmorel8@gmail.com>
2018-07-04 21:58:26 -0400

    Def object

---
+++ assets/javascript/game.js
@@ -0,0 +1,94 @@
+
+
+var rpg = (function(){
+   
+var logic = (function() {
+        
+        var character;
+        var characterChosen;
+        var enemyChosen = false;
+        var opponent;
+        var defeated = false; 
+
+        var characters = [
+            darkV = {
+                name: "Dark Vader",
+                image: "<img src = '../images/darkvader.jpg' id = 'Vader'>",
+                counterAttack: 5,
+                attack: 5,
+                health: 50,
+           
+            },
+        
+            lukeS = {
+                name: "Luke Skywalker",
+                image: "<img src = 'assets/images/lukesky.jpg' id = 'Luke'>",
+                counterAttack: 6,
+                attack: 6,
+                health: 80,
+             
+            },
+        
+            sith = {
+                name: "Sith",
+                image: "<img src = 'assets/images/sith.jpg' id = 'Sith'>",
+                counterAttack: 8,
+                attack: 8,
+                health: 60,
+                
+            },
+        
+            oviwan = {
+                name: "Ovi-wan Kanovi",
+                image: "<img src = 'assets/images/oviwan.jpg' id = 'Ovi-Wan'>",
+                counterAttack: 10, 
+                attack: 10,
+                health: 40,
+                
+            }
+        ];
+
+        //private functions 
+
+
+    })
+
+        return {
+
+          init: function(){
+              
+
+
+          },
+
+          selectChar: function(arg) {
+
+          },
+
+          selectOpo: function(arg){
+
+
+          },
+
+          attackOp: function(){
+
+          },
+
+          reset: function(){
+
+          }
+
+
+
+          
+        };
+
+
+    })();
+
+        
+
+
+
+
+
